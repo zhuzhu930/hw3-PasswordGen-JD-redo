4f381b5097cc36e375b636cd19a836504b2c8643
Jessie Doherty <zhuzhu930@yahoo.com>
2022-04-28 18:39:19 -0500

    writing my plans

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//My Plan: 
+//1. when clicking the button to generate a password, a series of prompts pop up. Store the user inputs into variables. 
+
+//2. use arrays to store lowercase letters, uppercase letters, numerics, and special characters. 
+
+//3. write a function to generate the password. 
+// Based on the prompts answers, use .push() methods to construct new arrays that meet the requrement, once new arrays are constructed, generate random password base on the length the user chose. 
+
+//4. Once a password is generated, use starter code writePassword funciton to write it into the textbox. 
