a893a9588751d1aff0ebb6245b492124a4a047c4
Jessie Doherty <zhuzhu930@yahoo.com>
2022-04-28 22:51:59 -0500

    cleaning up javascript code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1. Define variables
 // Connecting the DOM
 const generateBtn = document.querySelector("#generate");
 const passwordText = document.querySelector("#password");
@@ -11,25 +12,14 @@ const lengthQ = parseInt(prompt("How long do you want your password to be? Pleas
 const passwordString = [];
 
 
-
-// const lowercaseQ = prompt("Do you want to include lowercase letters? Please answer Yes or No.");
-// const uppercaseQ = prompt("Do you want to include uppercase letters? Please answer Yes or No.");
-// const numericsQ = prompt("Do you want to include numbers? Please answer Yes or No.");
-// const specialCharQ = prompt("Do you want to include special characters? Please answer Yes or No.");
-// const lengthQ = parseInt(prompt("How long do you want your password to be? Please enter a number between 8 and 128 characters.")); 
-
-
-//My Plan: 
-
-
-//2. use arrays to store lowercase letters, uppercase letters, numerics, and special characters. 
+//2. Use arrays to store lowercase letters, uppercase letters, numerics, and special characters. 
 
 const lowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const numberArray = ["1","2","3","4","5","6","7","8","9","0"];
 const specialCharArray = ["~","!","@","#","$","%","^","&","*"];
 
-//3. Concatenate arrays based on user input: 
+//3. Concatenate arrays based on user's input: 
 //all options chosen
 const allArray = lowerArray.concat(upperArray).concat(numberArray).concat(specialCharArray); //condition done
 //only chosen 2 options
@@ -45,7 +35,7 @@ const lowerUpperSpecial = lowerArray.concat(upperArray).concat(specialCharArray)
 const lowerNumSpecial = lowerArray.concat(numberArray).concat(specialCharArray);
 const upperNumSpecial = upperArray.concat(numberArray).concat(specialCharArray);
 
-//4. Set a function to generate random string
+//4. Set a function to generate random string, I tested the conditions first before wrapping the entire condition with a function. 
 
 function generate() {
 //Adding conditions
@@ -130,36 +120,10 @@ if (uppercaseQ.toLowerCase() === "yes" && numericsQ.toLowerCase() === "yes" && s
 } 
 }
 
-// Add event listener to generate button
+//5. Adding an event listener to the generate button.
 generateBtn.addEventListener("click", () => {
+  //call generate function. 
   generate();
+  //writing the generated string into HTML
   passwordText.innerHTML = passwordString.join("");
 });
-
-//reference
-// for (let i = 0; i <= lengthQ; i++) {
-//   const randomCharacter = lowerArray[Math.floor(Math.random() * lowerArray.length)];
-//   passwordString.push(randomCharacter);
-// }
-//passwordText.innerHTML = passwordString.join("");
-
-
-
-
-//4. write a function to generate the password. 
-// Based on the prompts answers, use .push() methods to construct new arrays that meet the requrement, once new arrays are constructed, generate random password base on the length the user chose. 
-
-// function generatePasswor() {
-//   //setting conditions, if all, 2 or 3 options chosen
-// }
- 
-//4. Once a password is generated, use starter code writePassword funciton to write it into the textbox. 
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
